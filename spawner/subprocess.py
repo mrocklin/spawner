6b779287381bdf169b3d1edc09c7e1d484cefbdd
Matthew Rocklin <mrocklin@gmail.com>
2016-03-16 10:46:01 -0700

    add docstrings

--- spawner/subprocess.py
+++ spawner/subprocess.py
@@ -1,5 +1,7 @@
 import os
+import shutil
 import tempfile
+import time
 from threading import Thread, Event
 
 import tornado.process
@@ -29,7 +31,13 @@ class Job(object):
         self.on_finish = on_finish
         self.directory = directory or tempfile.mkdtemp(prefix='spawner-')
         self.process = start_process(self.args, self.directory, self.io_loop)
-        on_start()  # we start immediately, no waiting involved
+
+        while not os.path.exists(os.path.join(self.directory, 'pid')):
+            time.sleep(0.01)
+        on_start()  # we choose to block until start for processes
+        with open(os.path.join(self.directory, 'pid')) as f:
+            self.id = int(f.read())
+
         self.done_event = Event()
         self.status = 'running'
 
@@ -48,17 +56,16 @@ class Job(object):
         self.spawner.jobs.remove(self)
 
     def get_stdout(self):
+        """ Returns an iterator of lines of stdout, can be called repeatedly """
         with open(os.path.join(self.directory, 'log.out')) as f:
             for line in f:
                 yield line.rstrip()
 
     def get_stderr(self):
+        """ Returns an iterator of lines of stderr, can be called repeatedly """
         with open(os.path.join(self.directory, 'log.err')) as f:
             for line in f:
                 yield line.rstrip()
-
-    def id(self):
-        return self.process.pid
 
     @property
     def io_loop(self):
@@ -67,9 +74,35 @@ class Job(object):
     def join(self):
         self.done_event.wait()
 
+    def __del__(self):
+        try:
+            shutil.rmtree(self.directory)
+        except (IOError, OSError):
+            pass
+
 
 class LocalProcessSpawner(object):
-    """ Context to create and manage local jobs """
+    """ Context to create and manage local jobs
+
+    Examples
+    --------
+    >>> spawner = LocalProcessSpawner()
+
+    >>> job = spawner.spawn(['echo', 'hello, world!'])
+    >>> job.join()
+    >>> list(job.get_stdout())
+    ['hello, world!']
+
+    Optionally provide callbacks for start and finish
+
+    >>> log = []
+    >>> job = spawner.spawn(['echo', 'hello, world!'],
+    ...                     on_start=lambda: log.append('started'),
+    ...                     on_finish=lambda: log.append('finished'))
+    >>> job.join()
+    >>> log
+    ['started', 'finished']
+    """
     Job = Job
     def __init__(self, io_loop=None, directory=None):
         self.jobs = set()
@@ -82,11 +115,23 @@ class LocalProcessSpawner(object):
             sleep(0.001)
 
     def spawn(self, args, on_start=do_nothing, on_finish=do_nothing):
+        """ Spawn new job
+
+        Parameters
+        ----------
+        args: list
+            List of terms for command, like ``['sleep', '10']``
+        on_start: callable
+            Function to run when job starts
+        on_finish: callable
+            Function to run when job finishes
+        """
         job = Job(self, args, on_start=on_start, on_finish=on_finish)
         self.jobs.add(job)
         return job
 
     def close(self):
+        """ Close all active jobs """
         for job in list(self.jobs):
             job.kill()
 
